2c02ff6ca04261c2eb8ed20cbc8ad2a60ad1ddb2
Artur <71438694+ArturSharafutdinov@users.noreply.github.com>
2024-10-04 16:02:10 +0300

    add all necessary pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,32 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    {
+      name: 'home',
+      path: '/',
+      component: () => import('../views/home/Home.vue'),
+      meta: {
+        label: 'Главная',
+      }
+    },
+    {
+      name: 'basket',
+      path: '/basket',
+      component: () => import('../views/basket/Basket.vue'),
+      meta: {
+        label: 'Корзина',
+      }
+    },
+    {
+      name: 'login',
+      path: '/login',
+      component: () => import('../views/auth/Login.vue'),
+      meta: {
+        label: 'Авторизация',
+      }
+    }
+  ]
 })
 
 export default router
